89b5aaffc63a4b807bff3067c384cf023818a052
akshit-non <akshit@non.sa>
2022-08-05 13:24:11 +0530

    Minor changes - Enables cors - Fixes response structure

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
+app.use(cors());
 
 const usersRoute = require('./routes/users');
 
@@ -8,6 +10,14 @@ app.get('/', function (req, res) {
 });
 
 app.use('/users/v1/', usersRoute);
+
 app.listen(8000, function () {
   console.log('Listening to Port 8000');
+});
+
+app.use(function (req, res, next) {
+  console.log('executed');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
--- models/users.js
+++ models/users.js
@@ -11,16 +11,18 @@ const usersList =
     "1": {
         id: 1,
         name: 'Cristiano Ronaldo',
-        wallet_address: 'abc'
+        wallet_address: 'abc',
+        profile_pic: 'https://i.pinimg.com/originals/b8/69/5f/b8695f007aea9a08a0419479217ca6aa.jpg'
     },
     "2": {
         id: 2,
         name: 'Lionel Messi',
-        wallet_address: 'xyz'
+        wallet_address: 'xyz',
+        profile_pic: 'https://i.pinimg.com/originals/b8/69/5f/b8695f007aea9a08a0419479217ca6aa.jpg'
     }
 
 }
 
 exports.getUsers = async (userId) => {
-    return usersList[userId];
+    return [usersList[userId]];
 }
