b6460bd34622a35eaba6b4a4deb5ec618d8cba14
akshit-non <akshit@non.sa>
2022-08-05 15:11:49 +0530

    Minor fix - Updates user's public address

--- models/users.js
+++ models/users.js
@@ -11,7 +11,7 @@ const usersList =
     "1": {
         id: 1,
         name: 'Cristiano Ronaldo',
-        wallet_address: 'abc',
+        wallet_address: '0x259E6C78db09073D8f21230d519a2353AA189374',
         profile_pic: 'https://i.pinimg.com/originals/b8/69/5f/b8695f007aea9a08a0419479217ca6aa.jpg'
     },
     "2": {
